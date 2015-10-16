faf3b3fad6b0fe6b85a07378d257ba3a16b75e94
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2015-10-16 11:11:40 +0300

    edit.php edit form

--- new/edit.php
+++ new/edit.php
@@ -1,3 +1,16 @@
 <?php
-
+	
+	//id mida muudame
+	echo $_GET["edit"];
+	//vaja saada kätte kõige uuemad andmed id kohta
+	
 ?>
+
+<h2>Muuda auto</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+  	<label for="number_plate" >auto nr</label><br>
+	<input id="number_plate" name="number_plate" type="text" value=""><br><br>
+  	<label for="color">värv</label><br>
+	<input id="color" name="color" type="text" value=""><br><br>
+  	<input type="submit" name="update" value="Salvesta">
+  </form>
--- new/table.php
+++ new/table.php
@@ -18,6 +18,7 @@
 		<th>Värv</th>
 		<th>X</th>
 		<th>Edit</th>
+		<th>Edit fail</th>
 	</tr>
 	<?php
 			//iga massiivis oleva elemendi kohta, masiivi pikkus, $i++ = $i=$i+1
@@ -42,6 +43,7 @@
 				echo"<td>".$car_list[$i]->color."</td>";
 				echo"<td><a href='?delete=".$car_list[$i]->id."'>x</a></td>";
 				echo"<td><a href='?edit=".$car_list[$i]->id."'>edit</a></td>";
+				echo"<td><a href='edit.php?edit=".$car_list[$i]->id."'>edit.php</a></td>";
 				echo"</tr>";
 			}
 		}
